de68a0e20ac7749aa2d36ddfc1486d734522adff
DaMSoN <damian.wieczorek99@gmail.com>
2020-09-10 23:50:17 +0200

    adding root for orders. Closes #2

---
+++ src/main/java/mssc/beer/config/LocalHostRouteConfig.java
@@ -0,0 +1,26 @@
+package mssc.beer.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("localRots")
+public class LocalHostRouteConfig
+{
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder)
+    {
+        return builder.routes()
+                .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
+                .uri("http://localhost:8080")
+                .id("beer-service"))
+
+                .route(r -> r.path("/api/v1/customers/**")
+                .uri("http://localhost:8081")
+                .id("order-service"))
+                .build();
+    }
+}
